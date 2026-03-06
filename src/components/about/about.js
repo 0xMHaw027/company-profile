11612254d0cd208bed9292301b2df58c2b741c54
0xMHaw027 <viraham27@gmail.com>
2026-03-06 23:09:12 +0700

    fix: all image paths

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,44 +1,21 @@
-import "./about.scss";
+import "./footer.scss";
+import logoImage from "../../assets/images/logo.png"; // ← tambah ini
 
-const About = () => {
-  const about = document.createElement("section");
-  about.classList.add("about");
-  about.setAttribute("id", "about");
-
-  about.innerHTML = `
-    <div class="about__container">
-
-      <div class="about__image">
-        <img src="/src/assets/images/about.png" alt="About Illustration"/>
+const Footer = () => {
+  const footer = document.createElement("footer");
+  footer.classList.add("footer");
+  footer.innerHTML = `
+    <div class="footer__container">
+      <div class="footer__logo">
+        <img src="${logoImage}" alt="Logo"/>
+        <span class="footer__logo-text">0xMHaw</span>
       </div>
-
-      <div class="about__content">
-        <h2 class="about__title">About <strong>Me</strong></h2>
-
-        <div class="about__description">
-          <p>
-            I'm a passionate, self-proclaimed designer who specializes
-            in full stack development (React.js & Node.js). I am very
-            enthusiastic about bringing the technical and visual aspects
-            of digital products to life.
-          </p>
-          <p>
-            I began my journey as a web developer in 2015, and since then,
-            I've continued to grow and evolve as a developer, taking on new
-            challenges and learning the latest technologies along the way.
-          </p>
-          <p>
-            When I'm not in full-on developer mode, you can find me hovering
-            around on twitter or on indie hacker, witnessing the journey of
-            early startups or enjoying some free time.
-          </p>
-        </div>
+      <div class="footer__info">
+        <span class="footer__copyright">© 2025-2026 Personal</span>
+        <span class="footer__made">Made in Figma</span>
       </div>
-
     </div>
   `;
-
-  return about;
+  return footer;
 };
-
-export default About;
+export default Footer;
