5992902bccde70f06625e3c158124d2493ada45e
0xMHaw027 <viraham27@gmail.com>
2026-03-06 23:01:18 +0700

    fix: hero image path

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,5 +1,5 @@
 import "./hero.scss";
-import heroImage from "/src/assets/images/hero.png";
+import heroImage from "../../assets/images/hero.png"; // ← path benar
 
 const Hero = () => {
   const hero = document.createElement("section");
@@ -7,24 +7,22 @@ const Hero = () => {
 
   hero.innerHTML = `
     <div class="hero__container">
-
       <div class="hero__image">
-        <img src="/src/assets/images/hero.png" alt="Hero Illustration"/>
+        <img src="${heroImage}" alt="Hero Illustration"/>
       </div>
-
       <div class="hero__content">
         <span class="hero__subtitle">Welcome to my portfolio</span>
-
-       <h1 class="hero__title">
-  Hello I'm <strong><span class="hero__typed" id="typed-name"></span></strong><br/>
- AI-Focused <strong><span class="hero__typed hero__typed--outline" id="typed-role"></span></strong><br/>
-  Based In <strong><span class="hero__typed" id="typed-location"></span></strong>
-</h1>
-
+        <h1 class="hero__title">
+          Hello I'm <strong><span class="hero__typed" id="typed-name"></span></strong><br/>
+          AI-Focused <strong><span class="hero__typed hero__typed--outline" id="typed-role"></span></strong><br/>
+          Based In <strong><span class="hero__typed" id="typed-location"></span></strong>
+        </h1>
         <p class="hero__description">
-     Web Developer and AI Engineer focused on building modern web applications and exploring intelligent systems. Passionate about technology, problem-solving, and continuously learning to develop impactful AI-driven solutions.
+          Web Developer and AI Engineer focused on building modern web applications
+          and exploring intelligent systems. Passionate about technology,
+          problem-solving, and continuously learning to develop impactful
+          AI-driven solutions.
         </p>
-
         <div class="hero__social">
           <a href="#" class="hero__social-link" aria-label="Github" target="_blank">
             <i class="fa-brands fa-github"></i>
@@ -40,7 +38,6 @@ const Hero = () => {
           </a>
         </div>
       </div>
-
     </div>
   `;
 
@@ -68,7 +65,6 @@ const Hero = () => {
     typing();
   };
 
-  // Jalankan typing secara berurutan
   setTimeout(() => {
     typeText(typedName, "0xMHaw", () => {
       typeText(typedRole, "WebDEV", () => {
